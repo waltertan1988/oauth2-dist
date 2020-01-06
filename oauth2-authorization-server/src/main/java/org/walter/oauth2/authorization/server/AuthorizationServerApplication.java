1d1693e25b5a44ed5f69a14482e513884a505525
tanyunxing <tanyunxing@yy.com>
2020-01-06 22:07:53 +0800

    init version

---
+++ oauth2-authorization-server/src/main/java/org/walter/oauth2/authorization/server/AuthorizationServerApplication.java
@@ -0,0 +1,13 @@
+package org.walter.oauth2.authorization.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorizationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorizationServerApplication.class, args);
+	}
+
+}
---
+++ oauth2-authorization-server/src/test/java/org/walter/oauth2/authorization/server/AuthorizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.walter.oauth2.authorization.server;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
