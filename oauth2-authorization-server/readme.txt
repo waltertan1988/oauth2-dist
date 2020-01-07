0f59c3e5b24b384cedc28dfd03a3ee25751f35d8
tanyunxing <tanyunxing@yy.com>
2020-01-07 20:07:37 +0800

    引入资源服务器

--- oauth2-authorization-server/readme.txt
+++ oauth2-authorization-server/readme.txt
@@ -1,1 +1,4 @@
-http://localhost:7081/authServer/oauth/authorize?response_type=code&client_id=demo&redirect_uri=http://localhost:7081/authServer/resource&scope=all
+
+http://localhost:7081/authServer/oauth/authorize?response_type=code&client_id=demo&redirect_uri=http://www.baidu.com&scope=all
+
+http://localhost:7081/authServer/resource
--- oauth2-authorization-server/src/main/java/org/walter/oauth2/AuthorizationServerApplication.java
+++ oauth2-authorization-server/src/main/java/org/walter/oauth2/AuthorizationServerApplication.java
@@ -3,8 +3,10 @@ package org.walter.oauth2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
+@EnableResourceServer
 @EnableAuthorizationServer
 public class AuthorizationServerApplication {
 
