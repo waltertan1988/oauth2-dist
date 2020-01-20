36c04b4693be49a860b2d997587ab0bed5d0f476
tanyunxing <tanyunxing@yy.com>
2020-01-20 17:41:11 +0800

    移除ResourceServer，此服务仅仅做AuthorizationServer

--- oauth2-authorization-server/src/main/java/org/walter/oauth2/configuration/SecurityConfiguration.java
+++ oauth2-authorization-server/src/main/java/org/walter/oauth2/configuration/SecurityConfiguration.java
@@ -3,7 +3,6 @@ package org.walter.oauth2.configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -13,11 +12,9 @@ import org.walter.oauth2.properties.CustomSecurityProperties;
 import org.walter.oauth2.service.CustomHttp403ForbiddenEntryPoint;
 import org.walter.oauth2.service.CustomOauth2AuthenticationSuccessHandler;
 
-@Order(2) //保证基本的身份认证处理在oauth2授权前已经执行
 @Configuration
 @EnableAuthorizationServer
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-
     @Autowired
     private CustomSecurityProperties customSecurityProperties;
     @Autowired
