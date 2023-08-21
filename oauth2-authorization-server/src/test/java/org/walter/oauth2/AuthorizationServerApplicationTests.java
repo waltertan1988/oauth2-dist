74eb6d1f42a479912eaca83f8b2b03c8e120df8a
walter.tan <Walter.Tan@moco.com>
2023-08-21 09:35:03 +0800

    使用junit5

--- oauth2-authorization-server/src/test/java/org/walter/oauth2/AuthorizationServerApplicationTests.java
+++ oauth2-authorization-server/src/test/java/org/walter/oauth2/AuthorizationServerApplicationTests.java
@@ -1,13 +1,32 @@
 package org.walter.oauth2;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
-@SpringBootTest
+import javax.annotation.Resource;
+
+@Slf4j
+@SpringBootTest(classes = AuthorizationServerApplication.class)
 class AuthorizationServerApplicationTests {
 
+	@Resource(name = "userDetailsServiceImpl")
+	private UserDetailsService userDetailsService;
+
 	@Test
+	@Disabled
 	void contextLoads() {
 	}
 
+	@Test
+	void loadUserByUsername() {
+		String username = "0009785";
+		UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+		Assertions.assertNotNull(userDetails);
+		Assertions.assertEquals(username, userDetails.getUsername());
+	}
 }
--- oauth2-resource-server/src/test/java/org/walter/oauth2/ResourceServerApplicationTests.java
+++ oauth2-resource-server/src/test/java/org/walter/oauth2/ResourceServerApplicationTests.java
@@ -1,12 +1,14 @@
 package org.walter.oauth2;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = ResourceServerApplication.class)
 class ResourceServerApplicationTests {
 
 	@Test
+	@Disabled
 	void contextLoads() {
 	}
 
